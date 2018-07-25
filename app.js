ee60d2c0087baa4b747927409699a16c062247d3
Unknown <alyonabill3@mail.ru>
2018-07-25 15:58:51 +0300

    routes files was added

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const Promise = require('bluebird');
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
 const pages = require('./app/controllers/pages');
 const ejsLocals = require('ejs-locals');
+
 const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+require('./app/routes');
+
+const dbPromise = Promise.resolve()
+    .then(() => sqlite.open('./database.sqlite', { Promise }))
+    .then(db => db.migrate({ force: 'last' }));
+
+app.get('/post/:id', async (req, res, next) => {
+    try {
+        const db = await dbPromise;
+        const [post, categories] = await Promise.all([
+            db.get('SELECT * FROM Post WHERE id = ?', req.params.id),
+            db.all('SELECT * FROM Category')
+        ]);
+        res.render('post', { post, categories });
+    } catch (err) {
+        next(err);
+    }
+});
 
 app.set('views','./app/views');
 app.set('view options', { layout:'./app/views/layout.ejs' });
@@ -19,8 +45,8 @@ app.get('/home', pages.home);
 app.get('/features', pages.features);
 app.get('/news', pages.news);
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+    console.log('App listening on port 3000!');
 });
 
 module.exports = app;
