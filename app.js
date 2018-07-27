b28832fff102ccdf04e31dd1c8c0b2c82e5af345
Unknown <alyonabill3@mail.ru>
2018-07-27 17:17:42 +0300

    form for the reg users was added

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const Promise = require('bluebird');
 const sqlite = require('sqlite');
-const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
-
-const pages = require('./app/controllers/pages');
-const ejsLocals = require('ejs-locals');
+const engine = require('ejs-mate');
 
 const app = express();
+const pages = require(__dirname + '/app/controllers/pages');
 const port = process.env.PORT || 3000;
+const users = [];
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/app/views');
+app.set('view options', { layout:'./app/views/layout.ejs' });
+app.use('/static', express.static('static'));
+
+app.use(function (req, res, next) {
+    app.locals.route = req.url;
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./app/routes');
 
@@ -32,8 +44,8 @@ app.get('/post/:id', async (req, res, next) => {
 app.set('views','./app/views');
 app.set('view options', { layout:'./app/views/layout.ejs' });
 app.set('view engine', 'ejs');
-app.engine('ejs', ejsLocals);
-app.use(express.static('/static'));
+app.engine('ejs', engine);
+app.use(express.static(__dirname + '/static'));
 
 app.use(function (req, res, next) {
     app.locals.route = req.url;
@@ -45,8 +57,29 @@ app.get('/home', pages.home);
 app.get('/features', pages.features);
 app.get('/news', pages.news);
 
+app.post('/users', function (req, res) {
+    const user = req.body;
+    users.push({
+        id: users.id,
+        email: users.email,
+        name: user.name,
+        phone: users.phone,
+        birthDate: users.birth_date,
+        notesCount: users.notes_count
+    });
+    res.send('Successfully registered')
+});
+
+app.post('/users', function (req, res) {
+    const user = req.body;
+    fs.appendToFile('users.txt', JSON.stringify({name: user.name, age: user.age }), (err) => {
+        res.send('Successfully registered')
+    })
+});
+
 app.listen(port, function () {
     console.log('App listening on port 3000!');
 });
 
 module.exports = app;
+
