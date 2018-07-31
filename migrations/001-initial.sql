13c5f49ccd8580a494c2d36942652ff637189453
Unknown <alyonabill3@mail.ru>
2018-07-31 11:00:22 +0300

    fields notes_count and tags_count was added in script

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -6,6 +6,7 @@ CREATE TABLE user (
     password TEXT NOT NULL,
     name TEXT NOT NULL,
     phone INTEGER,
+    notes_count INTEGER,
     birth_date DATE
 );
 
@@ -14,6 +15,7 @@ CREATE TABLE note (
     body TEXT NOT NULL,
     author_id INTEGER NOT NULL,
     creation_date DATE NOT NULL,
+    tags_count INTEGER,
     FOREIGN KEY (author_id) REFERENCES user(id)
 );
 
@@ -25,7 +27,7 @@ CREATE TABLE tag (
 CREATE TABLE note_has_tag (
   note_id INTEGER PRIMARY KEY NOT NULL,
   tag_id INTEGER NOT NULL,
-    FOREIGN KEY (note_id) REFERENCES note(id),
+  FOREIGN KEY (note_id) REFERENCES note(id),
   FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
 
@@ -33,7 +35,7 @@ CREATE TABLE note_has_user_like (
   user_id INTEGER PRIMARY KEY NOT NULL,
   note_id INTEGER NOT NULL,
   state TINYINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (user_id) REFERENCES user(id),
   FOREIGN KEY (note_id) REFERENCES note(id)
 );
 
